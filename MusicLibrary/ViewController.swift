ec9bea6211b5ab47a69163d431a092add5cb7b74
Sorathia <krisna@trib3.com>
2015-01-17 15:47:56 -0800

    Initial Commit

---
+++ MusicLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicLibrary
+//
+//  Created by Krisna Sorathia on 1/17/15.
+//  Copyright (c) 2015 Krisna Sorathia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
